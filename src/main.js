2ab60b39333db3a5d82499acba24f315859321cb
yusuke-kobayashi <yusuke.kobayashi.yk@gmail.com>
2018-06-02 23:08:52 +0900

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  data: {
+    // message: 'Hello Vue.js!'
+    // list: ['りんご', 'ばなな', 'いちご']
+    // message: '初期メッセージ'
+    show: true
+  }
+  // methods: {
+  //   handleClick(event) {
+  //     alert(event.target)
+  //   }
+  // }
+});
